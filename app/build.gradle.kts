536f912cb0d1a147e3f0064bfc17f682bd51a1d5
sdlckdrl <sdlckdrl@kakao.com>
2026-04-23 16:52:28 +0900

    Bump version code for Play Console upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.fogbound.mafia"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.1"
     }
 
     signingConfigs {
